72f561c7c03d1394b21b4de3e60aa1e0ddc7ec50
Ravi Pandya <ravipandya151298@gmail.com>
2021-06-23 17:48:21 +0530

    Style Binding

--- hello-world/src/app/bindings/bindings.component.ts
+++ hello-world/src/app/bindings/bindings.component.ts
@@ -5,11 +5,18 @@ import { Component, OnInit } from '@angular/core';
   template: `<h1>
                 Binding Examples
               </h1>
+              <h1>Class Binding</h1>
               <h2 class="text-success">CodeEvaluation</h2>
               <h2 [class]="successClass">CodeEvaluation</h2>
               <h2 class="text-special" [class]="successClass">CodeEvaluation</h2>
               <h2 [class.text-danger] = "hasError">CodeEvaluation</h2>
               <h2 [ngClass] = "messageClasses">CodeEvaluation</h2>
+
+              <h1>Style Binding</h1>
+              <h2 style="color : blue">Style Binding 1</h2>
+              <h2 [style.color]= "'orange'">Style Binding 2</h2>
+              <h2 [style.color]= "hasError ? 'red' : 'orange'">Style Binding 3</h2>
+              <h2 [ngStyle]= "styleClasses">Style Binding 4</h2>
               `,
   styles: [`
   .text-success {
@@ -32,6 +39,10 @@ export class BindingsComponent implements OnInit {
     "text-success" : !this.hasError,
     "text-danger" : this.hasError,
     "text-special" : this.isSpecial
+  }
+  public styleClasses = {
+    color : "purple",
+    fontStyle : "Italic" 
   }
   constructor() { }
 
