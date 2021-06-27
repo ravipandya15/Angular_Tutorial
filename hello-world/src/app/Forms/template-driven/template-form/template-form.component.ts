53fef6d2ed158f23158ee186e720e8b0da0f38b0
Ravi Pandya <ravipandya151298@gmail.com>
2021-06-27 17:22:39 +0530

    Select control validation

--- hello-world/src/app/Forms/template-driven/template-form/template-form.component.ts
+++ hello-world/src/app/Forms/template-driven/template-form/template-form.component.ts
@@ -8,12 +8,24 @@ import { User } from '../user';
 })
 export class TemplateFormComponent implements OnInit {
 
-  topics = ['Angular', 'Vue', 'React'];
+  topics : string[] = ['Angular', 'Vue', 'React'];
+  topicHasError : boolean = true;
 
-  userModel = new User('','rpandya@hhaexchange.com',8511165331, '', 'morning', true);
+  userModel = new User('','rpandya@hhaexchange.com',8511165331, 'default', 'morning', true);
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  validateTopic(topicValue : string){
+    if (topicValue === "default"){
+      this.topicHasError = true;
+    }
+    else{
+      this.topicHasError = false;
+    }
+
+    //this.topicHasError = topicValue === "default" ? true : false;
   }
 
 }
